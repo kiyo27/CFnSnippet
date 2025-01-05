075cbec64b5d71f6274d420975e55a3a0c7144d0
kiyo27 <19531773+kiyo27@users.noreply.github.com>
2025-01-05 23:47:42 +0900

    update

---
+++ ecs/batch/src/app.py
@@ -0,0 +1,13 @@
+################################
+# image build command
+# 
+# docker pull python:3.12-alpine
+# docker build -t python-batch .
+# 
+################################
+
+def main():
+    print("hello world")
+
+if __name__ == "__main__":
+    main()
